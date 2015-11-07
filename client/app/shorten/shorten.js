bbd487a5fe3cf8b323cc0562f4cac0d6e39735cf
apiga <ambroise.piganeau@gmail.com>
2015-11-06 19:11:05 -0800

    displaying spinner and newly created links

--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -2,11 +2,15 @@ angular.module('shortly.shorten', [])
 
 .controller('ShortenController', function ($scope, $location, Links) {
   $scope.link = {};
+  $scope.display = false;
 
   $scope.addLink = function () {
+    $scope.display = true;
     Links.postLinkDB($scope.link)
-      .then(function (url) {
+      .then(function (data) {
+        $scope.display = false;
         $scope.link.url = '';
+        $scope.link.data = data;
       });
   };
 });
