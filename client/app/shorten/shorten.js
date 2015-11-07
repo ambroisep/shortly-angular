541a14a7e371178350e626b16b5327b9d6beb47e
apiga <ambroise.piganeau@gmail.com>
2015-11-06 17:23:07 -0800

    refactor to pass karma tests

--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -1,12 +1,12 @@
 angular.module('shortly.shorten', [])
 
 .controller('ShortenController', function ($scope, $location, Links) {
-  $scope.urlObj = {};
+  $scope.link = {};
 
-  $scope.createLink = function () {
-    Links.postLinkDB($scope.urlObj)
+  $scope.addLink = function () {
+    Links.postLinkDB($scope.link)
       .then(function (url) {
-        $scope.urlObj.url = '';
+        $scope.link.url = '';
       });
   };
 });
