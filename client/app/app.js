ac784834cae9c8599f8db6d0519d97f760026118
Ambroise Piganeau <ambroise.piganeau@gmail.com>
2015-11-07 11:47:15 -0800

    redirect to links when wrong route

--- client/app/app.js
+++ client/app/app.js
@@ -26,7 +26,11 @@ angular.module('shortly', [
     .when('/', {
       templateUrl: 'app/auth/signup.html',
       controller: 'AuthController'
-    });
+    })
+    .otherwise({
+        templateUrl: 'app/links/links.html',
+        controller: 'LinksController'
+      });
 
     // We add our $httpInterceptor into the array
     // of interceptors. Think of it like middleware for your ajax calls
